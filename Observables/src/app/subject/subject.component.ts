6adcbfd17a6750b7afc3676aa07ac3e711f897d3
shahnawaz1998 <shahnawaz199817@gmail.com>
2025-02-18 16:20:26 +0530

    AsyncSubject, ReplaySubject AND BehaviourSubject

--- Observables/src/app/subject/subject.component.ts
+++ Observables/src/app/subject/subject.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { AsyncSubject, BehaviorSubject, Observable, Subject } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
@@ -9,9 +9,13 @@ import { ajax } from 'rxjs/ajax';
   styleUrl: './subject.component.css'
 })
 export class SubjectComponent implements OnInit{
-  obs = ajax('https://randomuser.me/api/')
+  // obs = ajax('https://randomuser.me/api/')
   sub = new Subject()
+
+  myBehaviourSubject = new BehaviorSubject(10)
   ngOnInit():void{
+
+    //Difference Between Subject and Observable
 
     //Here we subscribed to observable three time and its return different values all time.
     // this.obs.subscribe((data)=>{
@@ -24,9 +28,28 @@ export class SubjectComponent implements OnInit{
     //   console.log(data)
     // })
 
-    this.obs.subscribe(this.sub)//Subject here is a consumer hence its called as multicaste
-    // console.log(this.sub)
-    this.sub.subscribe((data)=>{
+
+    //Multicaste Example of Subject
+
+    // this.obs.subscribe(this.sub)//Subject here is a consumer hence its called as multicaste
+    // // console.log(this.sub)
+    // this.sub.subscribe((data)=>{
+    //   console.log(data);
+      
+    // })
+    // this.sub.subscribe((data)=>{
+    //   console.log(data);
+      
+    // })
+    // this.sub.subscribe((data)=>{
+    //   console.log(data);
+      
+    // })
+
+
+
+    //Subject
+       this.sub.subscribe((data)=>{
       console.log(data);
       
     })
@@ -34,9 +57,37 @@ export class SubjectComponent implements OnInit{
       console.log(data);
       
     })
+    this.sub.next('Subject')
     this.sub.subscribe((data)=>{
       console.log(data);
       
     })
+
+    //Behaviour Subject
+    this.myBehaviourSubject.subscribe((data)=>{
+      console.log(data);
+    })
+    this.myBehaviourSubject.subscribe((data)=>{
+      console.log(data);
+    })
+    this.myBehaviourSubject.next(2020)
+
+    this.myBehaviourSubject.subscribe((data)=>{
+      console.log(data);
+    })
+
+
+    //Async Subject
+
+    const asyncSubject = new AsyncSubject();
+
+    asyncSubject.next(100);
+    asyncSubject.next(200);
+    asyncSubject.next(300);
+    asyncSubject.complete();
+
+    asyncSubject.subscribe(data=> console.log(data))
+    asyncSubject.subscribe(data=> console.log(data))
+    asyncSubject.subscribe(data=> console.log('Sub'+data))
   }
 }
