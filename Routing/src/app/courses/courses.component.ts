2e6826d76eb0b514931ec3b32cfaa7374e29db8c
shahnawaz1998 <shahnawaz199817@gmail.com>
2025-02-25 17:04:28 +0530

    quaryparams for search bar

--- Routing/src/app/courses/courses.component.ts
+++ Routing/src/app/courses/courses.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit, Pipe } from '@angular/core';
 import { CourseService } from '../Services/course.service';
 import { Course } from '../Models/course';
 import { CommonModule, CurrencyPipe } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -12,13 +12,24 @@ import { RouterLink } from '@angular/router';
   providers: [CurrencyPipe]
 })
 export class CoursesComponent implements OnInit{
-  AllCourses: Course[]  | undefined;
-  // coursesService = inject(CourseService);
-  constructor(private coursesService:CourseService ){
-  }
+  // AllCourses: Course[]  | undefined;
+  coursesService = inject(CourseService);
+  AllCourses:Course[] = this.coursesService.courses;
+  activeRoute:ActivatedRoute = inject(ActivatedRoute);
+  searchedQuery:any;
+
 
 ngOnInit(){
- this.AllCourses = this.coursesService.courses;
+  // this.searchedQuery = this.activeRoute.snapshot.queryParams['search'];
+ this.activeRoute.queryParamMap.subscribe((value:any)=>{ //We added this because if we have event on same page which changes the queryParamMap than observable queryParam will catch that and filter it out
+    this.searchedQuery = value.get('search');
+    if(this.searchedQuery == '' || this.searchedQuery == undefined ){
+      this.AllCourses = this.coursesService.courses;
+    }else{
+      this.AllCourses = this.coursesService.courses.filter(x=>x.title.toLowerCase().includes(this.searchedQuery.toLowerCase()))
+    }
+  })
+  
 }
 
 }
--- Routing/src/app/home/banner/banner.component.ts
+++ Routing/src/app/home/banner/banner.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-banner',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.css'
 })
 export class BannerComponent {
-
+  router:Router = inject(Router)
+  OnSearchClicked(data:string){
+    // console.log(data);
+    this.router.navigate(['/Courses'],{queryParams:{search:data}})
+  }
 }
