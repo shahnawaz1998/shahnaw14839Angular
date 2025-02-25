82303bb78061f53d0d85cfc1a33a0e71fdb47ac2
shahnawaz1998 <shahnawaz199817@gmail.com>
2025-02-25 17:31:23 +0530

    Fragments

--- Routing/src/app/home/home.component.ts
+++ Routing/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { BannerComponent } from './banner/banner.component';
 import { PopularComponent } from './popular/popular.component';
 import { TestimonyComponent } from './testimony/testimony.component';
@@ -6,6 +6,7 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
 import { ServicesComponent } from './services/services.component';
 import { CommonModule } from '@angular/common';
 import { ServicesService } from '../Services/services.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -14,6 +15,16 @@ import { ServicesService } from '../Services/services.service';
   styleUrl: './home.component.css',
   providers:[ServicesService]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  activeRoute:ActivatedRoute = inject(ActivatedRoute);
+  ngOnInit(): void {
+    this.activeRoute.fragment.subscribe((data)=>{
+      // console.log(data)
+      this.jumpToSection(data)
+    })
+  }
 
+  jumpToSection(section:any){
+    document.getElementById(section)?.scrollIntoView({behavior:'smooth'})
+  }
 }
